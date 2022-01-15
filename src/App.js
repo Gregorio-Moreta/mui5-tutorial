20afa59573d6c9b1080a46ba133f677bb6c5223d
Gregorio Moreta <gregoriomoreta4@gmail.com>
2022-01-15 09:03:56 -0500

    Imported mui5 components grid, and container. Used in App.js and TourCard.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import TourCard from './components/TourCard';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid'
 
 function App() {
   return (
     <div className="App">
-      <TourCard />
+      <Container>
+        <Grid container space={5}>
+        <TourCard />
+        <TourCard />
+        <TourCard />
+        <TourCard />
+        </Grid>
+      </Container>
     </div>
   );
 }
