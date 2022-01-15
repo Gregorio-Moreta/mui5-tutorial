06b377b5ee3286350f64381e5ece006e8d6ac5d4
Gregorio Moreta <gregoriomoreta4@gmail.com>
2022-01-15 08:47:25 -0500

    Basic set up/ mui5

---
+++ src/components/TourCard.js
@@ -0,0 +1,9 @@
+import Paper from '@mui/material/Paper';
+
+
+const TourCard = () => {
+    // variant="outlined", squared, rounded  --Props that can be passed to the paper component
+    return <Paper elevation={3}>Hello</Paper>
+}
+
+export default TourCard
